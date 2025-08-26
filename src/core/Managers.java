35805d2274bf8485c8080052be48ca55db62ad2f
VitaliiGrinchuk <gampirat@yandex.com>
2025-08-26 15:56:37 +0300

    2025-08-26 ФЗ-6: Выполнение ФЗ для 6 спринта. Девятая  версия, Исрпавление ошибок стиля.

--- src/core/Managers.java
+++ src/core/Managers.java
@@ -1,12 +1,13 @@
 package core;
 
 public final class Managers {
-    private Managers(){}
+    private Managers() { }
 
     public static TaskManager getDefault() {
 
         return new InMemoryTaskManager();
     }
+
     public static HistoryManager getDefaultHistory() {
 
         return new InMemoryHistoryManager();
