adb15ae32ba0c7493aceee0d555560338ca9c013
VitaliiGrinchuk <gampirat@yandex.com>
2025-08-26 15:47:02 +0300

    2025-08-26 ФЗ-6: Выполнение ФЗ для 6 спринта. Четвёртая  версия, так как на GitHub была опять ошибка, связанная с пустими строками.

--- src/core/TaskManager.java
+++ src/core/TaskManager.java
@@ -7,28 +7,41 @@ import model.Task;
 import java.util.List;
 public interface TaskManager {
         List<Task> getTasks();
+
         List<Subtask> getSubtasks();
+
         List<Epic> getEpics();
+
         List<Subtask> getEpicSubtasks(int epicId);
 
         Task getTask(int id);
+
         Subtask getSubtask(int id);
+
         Epic getEpic(int id);
 
         int addNewTask(Task task);
+
         int addNewEpic(Epic epic);
+
         int addNewSubtask(Subtask subtask);
 
         void updateTask(Task task);
+
         void updateEpic(Epic epic);
+
         void updateSubtask(Subtask subtask);
 
         void deleteTask(int id);
+
         void deleteEpic(int id);
+
         void deleteSubtask(int id);
 
         void deleteTasks();
+
         void deleteEpics();
+
         void deleteSubtasks();
 
         List<Task> getHistory();
