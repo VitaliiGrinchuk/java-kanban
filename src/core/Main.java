09f7b0e6c6b5ee9cca2b023d6ef65ca23f87729e
VitaliiGrinchuk <gampirat@yandex.com>
2025-09-26 08:03:26 +0300

    2025-09-04 ФЗ-8: Выполнение ФЗ для 8 спринта. Версия с исправлением стиля.

--- src/core/Main.java
+++ src/core/Main.java
@@ -105,7 +105,7 @@ public class Main {
                 }
                 Subtask subtask = new Subtask(0, title, desc, status, epicId);
                 int subtaskId = manager.addNewSubtask(subtask);
-                System.out.println(subtaskId != -1 ? "Создана подзадача id=" + subtaskId:"Ошибка создания подзадачи");
+                System.out.println(subtaskId != -1 ? "Создана подзадача id=" + subtaskId: "Ошибка создания подзадачи");
                 break;
             default:
                 System.out.println("Ошибка типа задачи");
@@ -168,6 +168,7 @@ public class Main {
         System.out.println("Введите ID эпика:");
         int epicId = Integer.parseInt(scanner.nextLine());
         System.out.println(manager.getEpicSubtasks(epicId));
+        System.out.println("Эпик получен!");
     }
 }
 //пишу этот комментарий, так как слегка накосячил с версиями в гите(задание делал на другой машине на работе и залил всё в основную ветку) а в пачке я вас не нашёл.
