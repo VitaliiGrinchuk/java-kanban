da686c925537a9163a7a0a6773b676f8e898f392
VitaliiGrinchuk <gampirat@yandex.com>
2025-09-26 08:06:42 +0300

    2025-09-04 ФЗ-8: Выполнение ФЗ для 8 спринта. Версия с исправлением стиля.

--- src/core/InMemoryHistoryManager.java
+++ src/core/InMemoryHistoryManager.java
@@ -1,12 +1,13 @@
 package core;
 
 import model.Task;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
+
     private static final int MAX_HISTORY_SIZE = 10;
+
     private final List<Task> history = new ArrayList<>();
 
     @Override
@@ -21,6 +22,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     public List<Task> getHistory() {
         return new ArrayList<>(history);
     }
+
     @Override
     public void remove(int id) {
         history.removeIf(task -> task.getId() == id);
